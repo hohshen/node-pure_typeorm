4b8eb424903a448fa5fa0b407b6c95bb4554c34b
hohshen <hohshen@gmail.com>
2019-12-02 00:05:25 +0800

    connect typeorm

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import se from './routes/se';
 import { createConnection } from "typeorm";
-import { databaseProviders } from "./common/app-config";
-
 // createConnection().then(async connection => {
 const app = express();
 app.use(bodyParser.json());
@@ -16,11 +14,6 @@ app.get('/', (req, res) => {
 app.listen(52788, async () => {
   console.log(`server started at http://localhost:${52788}`);
 });
-// }).catch(error => console.log("TypeORM connection error: ", error));
-// /**
-// * Create connection to DB using configuration provided in 
-// * appconfig file.
-// */
 createConnection().then(async connection => {
   console.log("Connected to DB");
 
